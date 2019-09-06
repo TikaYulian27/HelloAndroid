fe979c4225827f7da3dfd9a95279bec8df9ac77a
TikaYulian27 <tikayulian27@gmail.com>
2019-09-06 22:29:45 +0700

    Menyelesaikan BAB Android Activity Lifecycle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,3 +12,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(context: this, text: "App on start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(context: this, text: "App on Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Tost.makeText (context: this, text: "App on Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Tost.makeText (context: this, text: "App on Resume", Toast.LENGTH_SHORT).show();
+
+        @Override
+        protected void onPause(){
+            super.onPause();
+            Tost.makeText (context: this, text: "App on Pause", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onDestroy(){
+            super.onDestroyt();
+            Tost.makeText (context: this, text: "App on Destroy", Toast.LENGTH_SHORT).show();
+        }
